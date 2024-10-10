c5427d2f3e484a068f9f6b48f3da5e1c94a69600
Коммерческие курсы <comcuru@teach.local>
2024-10-10 14:48:37 +0300

    Initial commit

---
+++ SportInventoryApp/SportInventoryApp/Pages/EditPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportInventoryApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditPage.xaml
+    /// </summary>
+    public partial class EditPage : Page
+    {
+        public bool IsAdding = false;
+        public Models.Users CurrentUser = null;
+        public EditPage(Models.Users user, bool flag)
+        {
+            InitializeComponent();
+            IsAdding = flag;
+            if (user != null)
+            {
+                CurrentUser = user;
+            }
+        }
+
+        public void Init()
+        {
+            var Roles = Models.SportInventoryDBEntities.GetContext().Role.ToList();
+            Roles.Insert(0, new Models.Role() { Name = "Выберите роль"});
+            if (IsAdding != false)
+            {
+                
+            }
+        }
+
+        public void Update()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Utils.Navigation.ActiveFrame.CanGoBack)
+            {
+                Utils.Navigation.ActiveFrame.GoBack();
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SportInventoryApp/SportInventoryApp/Pages/ListViewPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportInventoryApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListViewPage.xaml
+    /// </summary>
+    public partial class ListViewPage : Page
+    {
+        public ListViewPage()
+        {
+            InitializeComponent();
+            UsersListView.ItemsSource = Models.SportInventoryDBEntities.GetContext().Users.ToList();
+        }
+
+        private void AddNewUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.Navigation.ActiveFrame.Navigate(new Pages.EditPage());
+        }
+
+        private void EditUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.Navigation.ActiveFrame.Navigate(new Pages.EditPage());
+        }
+    }
+}
